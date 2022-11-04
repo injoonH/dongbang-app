9ba1ed9264887819dd207953492d36807dde59ab
Injoon Hwang <injoon0617@gmail.com>
2022-11-04 21:24:31 +0900

    ✨ add bottom navigation bar with three tabs

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,53 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Dongbang',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Dongbang'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text("People"),
+    Text("Apps"),
+    Text("More"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dongbang'),
+        backgroundColor: const Color(0xffeba02a),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'People'),
+          BottomNavigationBarItem(icon: Icon(Icons.apps), label: 'Apps'),
+          BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: 'More'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xffeba02a),
+        onTap: _onItemTapped,
       ),
     );
   }
